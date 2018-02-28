e615aede37b5b5110385e44880050dfc5959a0c9
Cameron McCullers <crmbionicle@gmail.com>
2018-02-28 18:43:17 -0500

    Initial testing

---
+++ Project Outlove/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D rb2d;
+    private int count;
+	
+	public float speed;
+	public Text countText;
+	public Text winText;
+	
+
+	void Start(){
+		rb2d = GetComponent<Rigidbody2D>();
+		count = 0;
+		setCountText();
+		winText.text = "";
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce(movement*speed);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+		if(other.gameObject.CompareTag("PickUp")){
+			other.gameObject.SetActive(false);
+			count++;
+			setCountText();
+			if(count == 8){
+				winText.text = "YOU WIN";
+			}
+		}
+    }
+
+	void setCountText(){
+        countText.text = "Count: " + count.ToString();
+	}
+}
