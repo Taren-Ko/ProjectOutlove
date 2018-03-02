1e7130af41a97a664ac5c9d87247a3fe61580ce7
Cameron McCullers <crmbionicle@gmail.com>
2018-03-02 18:55:32 -0500

    Altered movement, begun jumping

--- Project Outlove/Assets/Scripts/PlayerController.cs
+++ Project Outlove/Assets/Scripts/PlayerController.cs
@@ -1,43 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-	private Rigidbody2D rb2d;
-    private int count;
-	
-	public float speed;
-	public Text countText;
-	public Text winText;
-	
+	[HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
 
-	void Start(){
-		rb2d = GetComponent<Rigidbody2D>();
-		count = 0;
-		setCountText();
-		winText.text = "";
-	}
-	void FixedUpdate(){
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce(movement*speed);
-	}
 
-    void OnTriggerEnter2D(Collider2D other)
+    private bool grounded = false;
+    private Rigidbody2D rb2d;
+
+
+    // Use this for initialization
+    void Start()
     {
-		if(other.gameObject.CompareTag("PickUp")){
-			other.gameObject.SetActive(false);
-			count++;
-			setCountText();
-			if(count == 8){
-				winText.text = "YOU WIN";
-			}
-		}
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-	void setCountText(){
-        countText.text = "Count: " + count.ToString();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+
+        if (h * rb2d.velocity.x < maxSpeed)
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+    }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
