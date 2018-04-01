2b93a6a8da52a8b052b9de7a7e286ac6ad61773e
rjp1215 <32282852+rjp1215@users.noreply.github.com>
2018-04-01 15:47:12 -0400

    Lever scripts

---
+++ Project Outlove/Assets/Scripts/LeverSprite.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class LeverSprite : MonoBehaviour {
+
+	//public Sprite sprite1;
+	//public Sprite sprite2;
+
+	private SpriteRenderer spriteRenderer; 
+	private bool canFlip;
+
+	void Start ()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+		//if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
+			//spriteRenderer.sprite = sprite1; // set the sprite to sprite1
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player")){
+			canFlip = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player")){
+			canFlip = false;
+		}
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Z) && canFlip)
+			{
+			if (spriteRenderer.flipX == true) {
+				spriteRenderer.flipX = false;
+			} else {
+				spriteRenderer.flipX = true;
+			}
+		}
+	}
+
+}
