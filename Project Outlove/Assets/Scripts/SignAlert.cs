839bb2eeab675f0e754c381680e5b14db95b6e51
Cameron McCullers <crmbionicle@gmail.com>
2018-05-06 14:08:09 -0400

    Added Sign Functionality

---
+++ Project Outlove/Assets/Scripts/SignAlert.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignAlert : MonoBehaviour {
+
+	public Text helpText;
+	public string displayText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")){
+            setInfoText(1);
+        }
+    }
+
+	void OnTriggerExit2D(Collider2D other){
+		setInfoText(0);
+	}
+
+	void setInfoText(int text){
+		if(text==1){
+			helpText.text=displayText;
+		}
+		else{
+			helpText.text="";
+		}
+	}
+}
